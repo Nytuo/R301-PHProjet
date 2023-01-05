14f7dddc5132d13cd6040130bbd70c10eafb56d1
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2023-01-05 22:48:59 +0100

    connexion

--- PHPPR/connexion.php
+++ PHPPR/connexion.php
@@ -1,22 +1,32 @@
 <?php
-include_once "head.php"
+include_once "head.php";
+include_once "header.php";
 ?>
-    <div>
-    <!-- conexion form -->
+    <main>
+        <h1>COMICS SANS ADMIN</h1>
+        <!-- conexion form -->
 
-    <form action="adminZonne.php" method="post">
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" required>
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" required>
-        <input type="submit" value="Connexion">
-    </form>
+        <form action="adminZonne.php" method="post">
+            <div class="input-field">
 
-</div>
+            <input type="email" name="email" id="email" class="validate" required>
+            <label for="email">Email</label>
+            </div>
+            <div class="input-field">
+            <input type="password" name="password" id="password" class="validate" required>
+            <label for="password">Password</label>
+
+            </div>
+            <input type="submit" class="btn waves-effect" value="Connexion">
+        </form>
+
+    </main>
 
 <?php
 //open session
 session_start();
-echo "<p style='color: red'>".$_SESSION['error']."</p>";
+echo "<p style='color: red'>" . $_SESSION['error'] . "</p>";
 $_SESSION['error'] = "";
+
+require_once "footer.php";
 
